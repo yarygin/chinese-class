82ffb564f2f1897922118df568e721f4eb161432
Alexander Yarygin <yarygin.alexander@inbox.ru>
2015-03-26 15:03:00 +0300

    cleaning

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,41 +1,39 @@
-<html>
-	<head>
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+@extends('app')
+@section('style')
+<style>
+	.container {
+		margin: 0;
+		padding: 0;
+		width: 100%;
+		height: 100%;
+		color: #B0BEC5;
+		display: table;
+		font-weight: 100;
+		font-family: 'Lato';
+	}
 
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
+	.content {
+		text-align: center;
+		display: table-cell;
+		vertical-align: middle;
+	}
 
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
+	.title {
+		text-align: center;
+		display: inline-block;
+	}
 
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
+	.title {
+		font-size: 72px;
+		margin-bottom: 40px;
+	}
+</style>
+@endsection
 
-			.title {
-				font-size: 72px;
-				margin-bottom: 40px;
-			}
-		</style>
-	</head>
-	<body>
+@section('content')
 		<div class="container">
 			<div class="content">
 				<div class="title">Нет такой страницы.</div>
 			</div>
 		</div>
-	</body>
-</html>
+@endsection
